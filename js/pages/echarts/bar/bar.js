4420c6de22c321a20bbcf0682e29e4c402793f1f
Hasan Sheikh <sheikharthur@gmail.com>
2019-12-03 23:04:36 +0600

    added eye_doctor_dashboard

--- js/pages/echarts/bar/bar.js
+++ js/pages/echarts/bar/bar.js
@@ -38,7 +38,7 @@ $(function() {
                 xAxis : [
                     {
                         type : 'category',
-                        data : ['Jan','Feb','Mar','Apr','May','Jun','July','Aug','Sept','Oct','Nov','Dec']
+                        data : ['01','02','03','04','05','06','07','08','09','10','11','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30']
                     }
                 ],
                 yAxis : [
@@ -50,7 +50,7 @@ $(function() {
                     {
                         name:'Site A',
                         type:'bar',
-                        data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+                        data:[50,20,50,23,22,20,60,33,23,30,44,33,20,38,23,10,12,50,40,12,10,5,3.30,33,34,50,33,20,32,12,5],
                         markPoint : {
                             data : [
                                 {type : 'max', name: 'Max'},
@@ -63,22 +63,22 @@ $(function() {
                             ]
                         }
                     },
-                    {
-                        name:'Site B',
-                        type:'bar',
-                        data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
-                        markPoint : {
-                            data : [
-                                {name : 'The highest year', value : 182.2, xAxis: 7, yAxis: 183, symbolSize:18},
-                                {name : 'Year minimum', value : 2.3, xAxis: 11, yAxis: 3}
-                            ]
-                        },
-                        markLine : {
-                            data : [
-                                {type : 'average', name : 'Average'}
-                            ]
-                        }
-                    }
+                    // {
+                    //     name:'Site B',
+                    //     type:'bar',
+                    //     data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+                    //     markPoint : {
+                    //         data : [
+                    //             {name : 'The highest year', value : 182.2, xAxis: 7, yAxis: 183, symbolSize:18},
+                    //             {name : 'Year minimum', value : 2.3, xAxis: 11, yAxis: 3}
+                    //         ]
+                    //     },
+                    //     markLine : {
+                    //         data : [
+                    //             {type : 'average', name : 'Average'}
+                    //         ]
+                    //     }
+                    // }
                 ]
             };
         // use configuration item and data specified to show chart
